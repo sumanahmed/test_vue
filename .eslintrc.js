b962c85b1a37cfec5dda1408408f47d8b834b654
sumanahmed <sumanahmed777333@gmail.com>
2020-02-19 10:02:31 +0600

    eslintrc modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
-"eslintConfig": {
-    ...
-    "rules": {
-      "no-console": "off"
+module.exports = {
+    'rules': {
+        'no-console': 'off',
     },
-}
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    extends: [
+        // add more generic rulesets here, such as:
+        // 'eslint:recommended',
+        'plugin:vue/recommended'
+    ],
+};
